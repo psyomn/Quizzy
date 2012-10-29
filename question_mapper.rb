d23405859b32b701036d28f7c2f8276aee50f72a
psyomn <lethaljellybean@gmail.com>
2012-10-29 02:48:17 -0400

    The mapper works. That's odd

--- question_mapper.rb
+++ question_mapper.rb
@@ -1,20 +1,34 @@
+require_relative 'question_tdg.rb'
+
 # Question Mapper to handle the stuff given from QuestionTDG
 # Author::Simon Symeonidis 
 class QuestionMapper
 
   def self.insert(question)
+    QuestionTDG
+      .insert(question.text,question.correct_answer_id)
   end 
 
   def self.update(question)
+    QuestionTDG
+      .update(question.id\
+      ,question.text\
+      ,question.correct_answer_id)
   end 
 
   def self.delete(question)
+    QuestionTDG
+      .delete(question.id)
   end 
 
   def self.find(id)
+    QuestionTDG
+      .find(id)
   end 
 
   def self.find_all
+    QuestionTDG
+      .find_all
   end 
 
 end 
