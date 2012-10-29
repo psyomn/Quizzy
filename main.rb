7240d8a30a169bfedc62b49394bbd98bea682c4a
psyomn <lethaljellybean@gmail.com>
2012-10-29 00:10:18 -0400

    Added necessary part where the table manager should be called in the beginning of the main entry point of the program.

--- main.rb
+++ main.rb
@@ -1,6 +1,11 @@
 require_relative 'command_line_interface.rb'
 require_relative 'table_manager.rb'
 
+# This line should always run before the application
+TableManager.run
+
+# Application code after this line 
+
 cli = CommandLineInterface.new 
 cli.run
 
