56d0a42959011de8f39d67192cc324a0470a98db
psyomn <lethaljellybean@gmail.com>
2012-10-29 02:48:24 -0400

    Fixed some SQL mistakes.

--- question_tdg.rb
+++ question_tdg.rb
@@ -4,25 +4,41 @@ require_relative 'database_registry.rb'
 # Author::Simon Symeonidis
 class QuestionTDG
 
-  @@insert_sql   = "" 
-  @@update_sql   = ""
-  @@delete_sql   = "" 
-  @@find_sql     = "SELECT * FROM questions WHERE id=?;"
-  @@find_all_sql = "SELECT * FROM questions;"
+  @@table_name   = "questions"
+  @@insert_sql   = "INSERT INTO #{@@table_name} (question_text,correct_answer_id) VALUES (?,?);" 
+  @@update_sql   = "UPDATE #{@@table_name} set question_text=?, correct_answer_id=? WHERE id=?;"
+  @@delete_sql   = "DELETE FROM #{@@table_name} WHERE id=?" 
+  @@find_sql     = "SELECT * FROM #{@@table_name} WHERE id=?;"
+  @@find_all_sql = "SELECT * FROM #{@@table_name};"
 
-  def self.insert(question)
+  def self.insert(question_text,answer_id)
+    DatabaseRegistry
+      .instance
+      .execute_prepared(@@insert_sql,[question_text,answer_id])
   end 
 
-  def self.update(question)
+  def self.update(id,question_text,answer_id)
+    DatabaseRegistry
+      .instance
+      .execute_prepared(@@update_sql,[question_text,answer_id,id])
   end 
 
-  def self.delete(question)
+  def self.delete(id)
+    DatabaseRegistry
+      .instance
+      .execute_prepared(@@delete_sql,[id])
   end 
 
   def self.find(id)
+    DatabaseRegistry
+      .instance
+      .execute_prepared(@@find_sql,[id])
   end 
 
   def self.find_all
+    DatabaseRegistry
+      .instance
+      .execute(@@find_all) 
   end 
 end 
 
