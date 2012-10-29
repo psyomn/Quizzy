954e6fb63e31a793c59cf2b131eeb6c5dbf23cd2
psyomn <lethaljellybean@gmail.com>
2012-10-29 00:10:34 -0400

    Some static data that the whole application uses

--- static_data.rb
+++ static_data.rb
@@ -3,9 +3,13 @@ module StaticData
 
   # The application version. Set it from here (small app,
   # no point in excessive configuration files).
-  APPLICATION_VERSION = :"0.1"
+  APPLICATION_VERSION = "0.1"
 
-  AUTHORS = [:"Simon J.  Symeonidis"]
+  AUTHORS = ["Simon J.  Symeonidis"]
+
+  DATABASE_NAME = "quiz.db"
+
+  VERSION_HISTORY_TABLE_NAME = "table_version_history"
 
 end 
 
